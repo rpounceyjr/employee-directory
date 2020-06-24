5901d7d19d4c8cd2bd549e9883059d273dcd7c64
rpounceyjr <rpounceyjr@gmail.com>
2020-06-23 22:02:29 -0400

    added wrapper component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import './App.css';
-// import EmployeeTable from './components/EmployeeTable/index.js';
+// import EmployeeTable from './components/EmployeeTable';
 import EmployeeCard from './components/EmployeeCard';
 import employees from "./employees.json";
 import Title from "./components/Title";
 import SearchByName from "./components/SearchByName";
+import Wrapper from "./components/Wrapper"
 
 class App extends Component {
   // Setting this.state.employees to the employees json array
@@ -13,9 +14,7 @@ class App extends Component {
     search: ""
   };
 
-  // update search state to be value of event.target
   updateSearch = str => {
-
     // Updating the input's state
     this.setState({
       search: str
@@ -27,8 +26,8 @@ class App extends Component {
     this.filterEmployeesByName(this.state.search);
 
   }
+
   // function attached to button to filter employees by name
-  
   filterEmployeesByName = name => {
     const selectedEmployee = this.state.employees.filter(employee => employee.name === name);
     this.setState({
@@ -42,20 +41,22 @@ class App extends Component {
       <div>
         <Title>Employee Directory</Title>
         <SearchByName
-          updateSearch= {this.updateSearch}
-          handleFormSubmit= {this.handleFormSubmit}
-          search= {this.state.search}
+          updateSearch={this.updateSearch}
+          handleFormSubmit={this.handleFormSubmit}
+          search={this.state.search}
 
         />
-        {this.state.employees.map(employee => (
-          <EmployeeCard
-            id={employee.id}
-            name={employee.name}
-            image={employee.image}
-            position={employee.position}
-            department={employee.department}
-          />
-        ))}
+        <Wrapper>
+          {this.state.employees.map(employee => (
+            <EmployeeCard
+              id={employee.id}
+              name={employee.name}
+              image={employee.image}
+              position={employee.position}
+              department={employee.department}
+            />
+          ))}
+        </Wrapper>
       </div>
     );
   }
